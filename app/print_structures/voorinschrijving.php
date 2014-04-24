862eda019dddff994d78d807fb498729732f0051
Michael Cleerbaut <michael.cleerbaut@hotmail.com>
2014-04-24 21:07:57 +0200

    Omschrijving telefonisch contact (print voorinschrijving) datum en uur erbij gezet om zelf in te kunnen vullen

--- app/print_structures/voorinschrijving.php
+++ app/print_structures/voorinschrijving.php
@@ -71,7 +71,7 @@
     $volgnummerB = $leerling['stroom'] == "A" ? "" : $volgnummerB;
 
     if($leerling['dag'] == "tel"){
-        $afspraak = "<span style=\"font-size:16px;font-weight:bold;\">U zal zelf de school contacteren om een afspraak te maken</span>";   
+        $afspraak = "<span style=\"font-size:16px;font-weight:bold;\">U zal zelf de school contacteren om een afspraak te maken</span><br><br> Datum: &nbsp;&nbsp; ....  / .... / ........ &nbsp;&nbsp;&nbsp;&nbsp;Uur: &nbsp;.... : ....";   
     } else if ($leerling['dag'] == "broerofzus"){
         $afspraak = "<span style=\"font-size:16px;font-weight:bold;\">U heeft reeds een afspraak gemaakt (met een broer of zus)</span>";   
     } else {
